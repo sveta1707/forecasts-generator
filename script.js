3307a370ba45f7aa0d96dd6d2d5d0afffea84deb
sveta1707 <svetlanakrasovskaa3@gmail.com>
2024-06-10 05:14:40 +0900

    Update script.js
    сгенерировала предсказания при клике  на кнопку

--- script.js
+++ script.js
@@ -13,3 +13,70 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const button = document.querySelector('.forecast-btn');
+
+function generateRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+
+button.addEventListener('click', function() {
+    let predictionText = "";
+
+    const randomNumber = generateRandomNumber(0, 6);
+    const probability = generateRandomNumber(0, 101);
+
+if (randomNumber == 1) {
+    predictionText = "Ваше сердце скоро вспыхнет от любви.";
+    } else if (randomNumber == 2) {
+    predictionText = "Вы скоро найдете новую работу, которая принесет вам успех.";
+    } else if (randomNumber == 3) {
+    predictionText = "На этой неделе вас ожидает приятное событие, которое долго будет радовать вас.";
+    } else if (randomNumber == 4) {
+    predictionText = "Ваше здоровье улучшится, если начнете заниматься спортом и правильно питаться.";
+    } else if (randomNumber == 5) {
+    predictionText = "Возможен финансовый успех, если вы проявите настойчивость.";
+    }
+
+    const newPrediction = document.querySelector('h1');
+    newPrediction.textContent = predictionText;
+
+    const newProbability = document.querySelector('p');
+    newProbability.textContent = probability;
+
+    const forecasts = document.querySelector('.forecasts');
+    const template = document.getElementById('forecast-item');
+
+    const item = template.content.cloneNode(true);
+
+    item.querySelector('h3').textContent = predictionText;
+    item.querySelector('p').textContent = probability;
+
+    forecasts.append(item);
+
+
+});
+
+
+
+
+
+    
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
